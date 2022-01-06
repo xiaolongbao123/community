b8f2f00e02f13cf6d907b5d703a78c1cdaaffbb8
zpn <208932039@qq.com>
2022-01-06 14:24:57 +0800

    add index and bootstrap

--- src/main/java/life/majiang/community/community/CommunityApplication.java
+++ src/main/java/life/majiang/community/community/CommunityApplication.java
@@ -3,6 +3,7 @@ package life.majiang.community.community;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//SpringBootApplication注解标识CommunityApplication类是springboot的启动类
 @SpringBootApplication
 public class CommunityApplication {
 
